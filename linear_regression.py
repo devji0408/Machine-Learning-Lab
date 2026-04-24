cd26b8483c0f4be022f52e6106390d7018db73aa
Panchdev Maddheshiya <panchdevmaddheshiya@gmail.com>
2026-04-24 13:04:07 +0530

    Modify linear regression example for salary prediction
    Updated the dataset to predict salary based on experience and added visualization.

--- linear_regression.py
+++ linear_regression.py
@@ -1,10 +1,25 @@
-from sklearn.linear_model import LinearRegression
+# Linear Regression Example - Predict Salary based on Experience
+
 import numpy as np
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
 
+# Dataset
 X = np.array([[1],[2],[3],[4],[5]])
-y = np.array([2,4,6,8,10])
+y = np.array([30000,40000,50000,60000,70000])
 
+# Model
 model = LinearRegression()
-model.fit(X,y)
+model.fit(X, y)
 
-print("Prediction for 6:", model.predict([[6]]))
+# Prediction
+pred = model.predict([[6]])
+print("Predicted Salary for 6 years experience:", pred)
+
+# Visualization
+plt.scatter(X, y, color='blue')
+plt.plot(X, model.predict(X), color='red')
+plt.xlabel("Experience")
+plt.ylabel("Salary")
+plt.title("Linear Regression")
+plt.show()
